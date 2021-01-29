eceeea6249d5b073c7091e98dd956537940d2764
scaur <james@txbatch.com>
2021-01-29 04:48:53 -0300

    Pass producer details as prop, download wax json on guild pageload

--- frontend/react-front/src/components/producer-detail.js
+++ frontend/react-front/src/components/producer-detail.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react'
+import axios from 'axios';
 import { Link } from 'react-router-dom';
 // Ununused var // import { makeStyles } from '@material-ui/core/styles';
 import TechresultTables from './tech-tablelist-results'
@@ -19,31 +20,46 @@ import Button from '@material-ui/core/Button';
   },
 })); */
 
-const App = ({ results ,products, bizdevs }) => {
+const App = ({ producer, results, products, bizdevs }) => {
   // Ununused var // const classes = useStyles();
-    return (
-      <div>
-
-        <ProdBizdev
-          results={ products }
-        />
-        <ProdBizdev
-          results={ bizdevs }
-        />
-        {/* // Ununused // <h1></h1> */}
-        <TechresultTables
-              results={ results }
-              description="Wax Mainnet"
-         />
-         <Link to={`/`}>
-          <Button variant="contained" color="primary" >
-            Back
-          </Button>
-         </Link>
-      </div>
-    )
+  const [waxJSON, setWaxJSON] = useState(null)
+  const [countryCode, setCountryCode] = useState(null)
   
+  if (producer && waxJSON === null) {
+    setWaxJSON({})
+    axios.get(producer.jsonurl).then(response => response.data).then(data => setWaxJSON(data))
   }
-  export default App
+
+  /* Use https://emoji-css.afeld.me/ or a custom map of country codes to flags? */
+  /*if (!countryCode && waxJSON !== null && waxJSON.org && waxJSON.org.location && waxJSON.org.location.country) {
+    setCountryCode(waxJSON.org.location.country)
+  }*/
+
+  return (
+    <div>
+      {/* Loads slowly for now - find more efficient solution */}
+      {/* producer ? <img alt={producer.candidate + " logo"} src={producer.logo_svg}/> : null */}
+      {producer ? <h1>{producer.owner_name} <small>{producer.candidate}</small></h1> : null}
+      <ProdBizdev
+        results={ products }
+      />
+      <ProdBizdev
+        results={ bizdevs }
+      />
+      {/* // Ununused // <h1></h1> */}
+      <TechresultTables
+            results={ results }
+            description="Wax Mainnet"
+        />
+        <Link to={`/`}>
+        <Button variant="contained" color="primary" >
+          Back
+        </Button>
+        </Link>
+    </div>
+  )
+  
+}
+export default App
 
 
