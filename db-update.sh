2aab7abc17bdb7fa23f98a783e50c0c499291c32
Charles Holtzkampf <charles.holtzkampf@gmail.com>
2021-07-20 09:48:09 +0100

    backend files re-added

--- db-update.sh
+++ db-update.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 ## Run update script passing name of container and master Postgresqpassword
-# EXample: ./update.sh oig-portal pgpassword
+## The script will copy over the updated backend files 
+# Example: ./update.sh oig-portal pgpassword
+
+dbupdate='True'
 
 # Copy over backend files
-sudo docker cp backend/* $2:/app/backend
+sudo docker cp backend/*.py $2:/app/backend
+sudo docker cp backend/*.sql $2:/app/backend
 
-
+# Perform a file content check on db_changes.sql
 # Set ENV & Update DB
-sudo docker exec -ti $2 export PGPASSWORD=$3
-sudo docker exec -ti $2 psql -d oig -W < db_changes.sql
+
+if dbupdate == 'True' then
+    sudo docker exec -ti $2 export PGPASSWORD=$3
+    sudo docker exec -ti $2 psql -d oig -W < db_changes.sql
+    sudo docker exec -ti $2 export PGPASSWORD='XZsacr%Drabbit!!'
+else then
+    echo "DB udadate not required"
+
+echo "Updates completed"
+
