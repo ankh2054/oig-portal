7ac099d1f277da70ca5d5fd111d7f378e4669107
Abdelhak Ahzab <abdou.ahzab@gmail.com>
2023-04-26 15:52:17 +0100

    [ADD] Guild scan popup message

--- frontend/react-front/src/features/guild-details/GuildInfo.tsx
+++ frontend/react-front/src/features/guild-details/GuildInfo.tsx
@@ -1,9 +1,12 @@
 import getUnicodeFlagIcon from 'country-flag-icons/unicode'
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 
+import useOnClickOutside from '../../hooks/useOnClickOutside'
 import { useLazyReScanQuery } from '../../services/api'
 import type { GuildResult, Producer } from '../../services/types'
 import Badge from '../../shared/badge/Badge'
+import IconClose from '../../shared/icons/IconClose'
+import IconInfo from '../../shared/icons/IconInfo'
 import IconNotion from '../../shared/icons/IconNotion'
 import Score from '../../shared/score/Score'
 
@@ -12,21 +15,27 @@ interface Props {
   result: GuildResult
 }
 const GuildInfo = ({ producer, result }: Props) => {
+  const scanPopupRef = useRef<HTMLDivElement>(null)
   const [rescan] = useLazyReScanQuery()
   const [scanMessage, setScanMessage] = useState('')
 
- const handleOnScanClick = async () => {
-  try {
-    const response = await rescan({ ownerName: producer.owner_name }).unwrap()
-    setScanMessage(response.message)
-    setTimeout(() => {
-      setScanMessage('')
-    }, 10000) // Hide the message after 10 seconds
-  } catch (error) {
-    console.error('Scan error:', error)
-    // Handle error case here, e.g., show an error message
+  const handleCloseScanPopup = () => {
+    setScanMessage('')
   }
-}
+
+  useOnClickOutside(scanPopupRef, handleCloseScanPopup)
+
+  const handleOnScanClick = async () => {
+    try {
+      const response = await rescan({ ownerName: producer.owner_name }).unwrap()
+      setScanMessage(response.message)
+      setTimeout(() => {
+        setScanMessage('')
+      }, 10000)
+    } catch (error) {
+      // Handle error case here, e.g., show an error message
+    }
+  }
 
   return (
     <>
@@ -64,14 +73,6 @@ const GuildInfo = ({ producer, result }: Props) => {
         >
           <IconNotion height="16" width="16" /> Notion
         </a>
-          {scanMessage && (
-            <div className="flex items-center bg-blue-500 text-white text-sm font-bold px-4 py-3" role="alert">
-              <svg className="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                <path d="M12.432 0c1.34 0 2.01.912 2.01 1.957 0 1.305-1.164 2.512-2.679 2.512-1.269 0-2.009-.75-1.974-1.99C9.789 1.436 10.67 0 12.432 0zM8.309 20c-1.058 0-1.833-.652-1.093-3.524l1.214-5.092c.211-.814.246-1.141 0-1.141-.317 0-1.689.562-2.502 1.117l-.528-.88c2.572-2.186 5.531-3.467 6.801-3.467 1.057 0 1.233 1.273.705 3.23l-1.391 5.352c-.246.945-.141 1.271.106 1.271.317 0 1.357-.392 2.379-1.207l.6.814C12.098 19.02 9.365 20 8.309 20z"/>
-              </svg>
-              <p>{scanMessage}</p>
-          </div>
-          )}
         <button
           onClick={handleOnScanClick}
           className="flex w-32 justify-center  rounded-full border bg-gradient-to-r from-redSalsa  to-sunsetOrange px-4 py-1 text-center text-sm font-medium text-white hover:border-primary hover:bg-primary  focus:outline-none"
@@ -79,6 +80,24 @@ const GuildInfo = ({ producer, result }: Props) => {
           Scan
         </button>
       </div>
+      {scanMessage && (
+        <div
+          ref={scanPopupRef}
+          className="absolute left-1/2 top-20 flex max-w-2xl -translate-x-1/2 transform  items-center bg-blue-500 py-3 pl-3 pr-7 text-sm font-bold text-white shadow-lg"
+          role="alert"
+        >
+          <div className="flex items-center gap-x-2">
+            <IconInfo color="white" />
+            <p>{scanMessage}</p>
+          </div>
+          <button
+            onClick={handleCloseScanPopup}
+            className="absolute right-1 top-1"
+          >
+            <IconClose color="white" />
+          </button>
+        </div>
+      )}
     </>
   )
 }
