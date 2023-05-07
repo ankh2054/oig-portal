f88cd413700a1456e3108c34f04405fa46ff2f88
Abdelhak Ahzab <abdou.ahzab@gmail.com>
2023-05-07 22:04:34 +0100

    [ADD] Added search guilds by owner name

--- frontend/react-front/src/features/latest-results/GuildsCheckResults.tsx
+++ frontend/react-front/src/features/latest-results/GuildsCheckResults.tsx
@@ -1,14 +1,15 @@
-import { useEffect, useState } from 'react'
+import type { ChangeEvent } from 'react'
+import { useState } from 'react'
 
 import type {
-  GuildResult,
   LatestResultsResponse,
   ResultsResponse,
   ProducersResponse,
   AvgResultsResponse,
+  GuildResult,
 } from '../../services/types'
 import GuildCard from '../../shared/guild-card/GuildCard'
-import Pagination from '../../shared/pagination/Pagination'
+import IconSearch from '../../shared/icons/IconSearch'
 import ResultsToggle from '../../shared/result-toggle/ResultsToggle'
 import { dayjs } from '../../utils/dates'
 import mapProducerToGuild from '../../utils/mapProducerToGuild'
@@ -24,8 +25,6 @@ interface Props {
   action?: JSX.Element
 }
 
-const ITEMS_PER_PAGE = 45
-
 const GuildsCheckResults = ({
   results,
   producers,
@@ -35,25 +34,9 @@ const GuildsCheckResults = ({
   action,
 }: Props) => {
   const [showAll, setShowAll] = useState(false)
-  const [paginatedData, setPaginatedData] = useState<Array<GuildResult>>([])
-  const [itemOffset, setItemOffset] = useState(0)
 
-  const [totalItems, setTotalItems] = useState(0)
   const onSwitch = (showAll: boolean) => {
     setShowAll(showAll)
-  }
-
-  useEffect(() => {
-    const activeGuilds = results.filter((r) => isActive(r.owner_name))
-    setTotalItems(activeGuilds.length)
-    setPaginatedData(
-      activeGuilds.slice(itemOffset, itemOffset + ITEMS_PER_PAGE)
-    )
-  }, [results, itemOffset, totalItems])
-
-  const handlePageClick = (activePage: number) => {
-    const newOffset = (activePage * ITEMS_PER_PAGE) % totalItems
-    setItemOffset(newOffset)
   }
 
   const isActive = (owner_name: string) => {
@@ -63,23 +46,62 @@ const GuildsCheckResults = ({
     return owner ? owner.active !== false && owner.active !== null : true
   }
 
+  const activeGuilds = results.filter((r) => isActive(r.owner_name))
+  const [guilds, setGuilds] = useState<Array<GuildResult>>(activeGuilds)
+
+  const search = (searchTerm: string) => {
+    setGuilds(
+      activeGuilds.filter((guild) =>
+        guild.owner_name.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+    )
+  }
+  const handleOnSearch = (e: ChangeEvent<HTMLInputElement>) => {
+    search(e.target.value)
+  }
+  const lastCheckFormattedTime = dayjs(results[0].date_check).format(
+    'DD MMMM H:mm'
+  )
   return (
     <div className="flex w-full flex-col">
       <div className="mb-4 flex justify-between">
         <h3 className="text-2xl">
           Latest results&nbsp;&nbsp;
           <span className="text-xl text-primary">
-            @ {dayjs(results[0].date_check).format('DD MMMM H:mm')}
+            @ {lastCheckFormattedTime}
           </span>
         </h3>
-        <div className="flex  gap-x-8">
-          <ResultsToggle onClick={onSwitch} showAll={showAll} />
-          {action}
+        <div className="flex gap-x-8">
+          <div>
+            <label
+              htmlFor="search"
+              className="text-gray-900 sr-only mb-2 text-sm font-medium"
+            >
+              Search
+            </label>
+            <div className="relative">
+              <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+                <IconSearch className="h-5 w-5 text-gray" />
+              </div>
+              <input
+                type="search"
+                id="search"
+                className="text-gray-900 border-gray-300 bg-gray-50 block w-full rounded-full border p-2 pl-10 text-sm focus:border-secondary focus:ring-secondary"
+                placeholder="Search"
+                onChange={handleOnSearch}
+                required
+              />
+            </div>
+          </div>
+          <div className="flex  gap-x-8">
+            <ResultsToggle onClick={onSwitch} showAll={showAll} />
+            {action}
+          </div>
         </div>
       </div>
       {avgResults && <AvgResults data={avgResults} showAll={showAll} />}
       <div className="grid gap-y-4">
-        {paginatedData.map((v, i) => {
+        {guilds.map((v, i) => {
           return (
             <GuildCard
               data={mapProducerToGuild(v, producers)}
@@ -91,14 +113,6 @@ const GuildsCheckResults = ({
           )
         })}
       </div>
-      {totalItems > ITEMS_PER_PAGE && (
-        <Pagination
-          className="mt-6 flex justify-center"
-          total={totalItems}
-          itemsPerPage={ITEMS_PER_PAGE}
-          onPageClick={handlePageClick}
-        />
-      )}
     </div>
   )
 }
