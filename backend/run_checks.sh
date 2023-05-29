030f771a3ee83baa025be52257652634222b1665
Charles Holtzkampf <charles.holtzkampf@gmail.com>
2023-05-29 19:16:42 +0100

    Updated the run_checks to ensure no duplicate process can be running.

--- backend/run_checks.sh
+++ backend/run_checks.sh
@@ -5,6 +5,21 @@
 # You can use command exit status in the shell script to display an error message or take some sort of action.
 
 COMMAND='/usr/bin/python3 /app/oig-portal/backend/start.py'
-ps -aux | grep $COMMAND >/dev/null && echo "Not running as process already running" || $COMMAND >> /app/python-oig.log 2>&1
+PIDFILE='/tmp/oig.pidc'
+LOGFILE='/app/python-oig.log'
+
+if [ -f $PIDFILE ]; then
+  PID=$(cat $PIDFILE)
+  if ps -p $PID > /dev/null; then
+    echo "Not running as process already running"
+    exit 1
+  else
+    # process not running, remove the stale pid file
+    rm $PIDFILE
+  fi
+fi
+
+$COMMAND >> $LOGFILE 2>&1 &
+echo $! > $PIDFILE
 
 
