c21c2a065bcb0eeccb7463011153608fd81badd2
Charles Holtzkampf <charles.holtzkampf@gmail.com>
2023-09-24 20:13:41 +0100

    added empty-blocks

--- frontend/react-front/src/pages/emptyblocks/EmptyBlocks.tsx
+++ frontend/react-front/src/pages/emptyblocks/EmptyBlocks.tsx
@@ -8,7 +8,7 @@ const EmptyBlocks = () => {
     })
     console.log(data);
     if (isLoading) return <div>Loading...</div>
-    if (error) return <div>Error: {error.message}</div>
+    if (error) return <div>Error: {error}</div>
   
     return (
       <table>
